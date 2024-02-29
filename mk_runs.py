07841e17d862758b654f379c63762a16a9b04163
Peter Teuben <teuben@gmail.com>
2024-02-29 04:21:18 +0000

    fix obsnums and pars

--- mk_runs.py
+++ mk_runs.py
@@ -13,20 +13,20 @@ on = {}
 
 on["B59"] =  [ 92634, 92825,]
 
-on["OH2"] = [ 92829, 92827, 92823, 92821, 92636, 92632, 92630]
+# on["OH2"] = [ 92829, 92827, 92823, 92821, 92636, 92632, 92630]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 
-pars1["B59"] = ""
-pars1["OH2"] = ""
+pars1["B59"] = "vlsr=185 dv=40 dw=80"
+# pars1["OH2"] = ""
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
 
-pars2["B59"] = ""
-pars2["OH2"] = ""
+pars2["B59"] = "pix_list=-0,1,4,5,12,13"
+#pars2["OH2"] = ""
 
 
 if __name__ == '__main__':    
